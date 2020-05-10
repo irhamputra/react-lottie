e95f189869de21c96c0b308c3b9f8af6dcac4170
Muhamad Irham Prasetyo <muhamad.prasetyo@get-ag.com>
2020-05-10 22:04:49 +0200

    useLottie.tsx done

--- src/useLottie.tsx
+++ src/useLottie.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
-import lottie, { AnimationConfig } from 'lottie-web';
+import lottie, { AnimationConfig, AnimationItem } from 'lottie-web';
 
-export const useLottie = (props: AnimationConfig) => {
-  const animationInstance = React.useRef(null);
-  const animationContainer = React.useRef<AnimationConfig | null>(null);
+export const useLottie = (props: AnimationConfig, style: object) => {
+  const animationInstance = React.useRef<AnimationItem | null>(null);
+  const animationContainer = React.useRef<HTMLDivElement | null>(null);
 
   React.useEffect(() => {
     animationInstance.current = lottie.loadAnimation({
@@ -12,13 +12,28 @@ export const useLottie = (props: AnimationConfig) => {
     }) as any;
   }, []);
 
-  const play = React.useCallback(() => {}, []);
+  const play = React.useCallback(() => {
+    if (animationInstance.current) {
+      animationInstance.current.play();
+    }
+  }, []);
 
-  const pause = React.useCallback(() => {}, []);
+  const pause = React.useCallback(() => {
+    if (animationInstance.current) {
+      animationInstance.current.pause();
+    }
+  }, []);
 
-  const stop = React.useCallback(() => {}, []);
+  const stop = React.useCallback(() => {
+    if (animationInstance.current) {
+      animationInstance.current.stop();
+    }
+  }, []);
+
+  const View = <div ref={animationContainer} style={style} />;
 
   return {
+    View,
     play,
     pause,
     stop,
